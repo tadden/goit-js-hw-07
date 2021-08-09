21007e9dcf38d71380a11b50c7c18592dc07f042
Ilja <tadden12@gmail.com>
2021-08-09 19:58:14 +0300

    complete task 1

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const mainList = document.querySelector('ul');
+
+const listItems = mainList.querySelectorAll('.item');
+
+console.log(`В списке ${listItems.length} категории`);
+
+const categoryTitle = mainList.getElementsByTagName('h2');
+listItems.forEach((el) => {
+    console.log('Категория : ', el.getElementsByTagName('h2')[0].textContent);
+    console.log('Количество элементов:', el.getElementsByTagName('li').length)
+})
